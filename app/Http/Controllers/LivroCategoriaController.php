c629669622c4faf3a63abffcd250ccd08349d266
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-12 19:05:50 -0400

    initial commit

---
+++ app/Http/Controllers/LivroCategoriaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LivroCategoria;
+use Illuminate\Http\Request;
+
+class LivroCategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LivroCategoria $livroCategoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LivroCategoria $livroCategoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LivroCategoria $livroCategoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LivroCategoria $livroCategoria)
+    {
+        //
+    }
+}
