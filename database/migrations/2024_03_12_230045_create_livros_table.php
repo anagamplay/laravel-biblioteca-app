68521697b444acb259c41c4f0bac4a13d6a70757
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-12 19:29:31 -0400

    migrations

--- database/migrations/2024_03_12_230045_create_livros_table.php
+++ database/migrations/2024_03_12_230045_create_livros_table.php
@@ -9,10 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // título, autor, editora, ano de publicação, categoria, número de exemplares disponíveis
     public function up(): void
     {
         Schema::create('livros', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('editora');
+            $table->integer('ano_de_publicacao');
+
+            $table->unsignedBigInteger('id_categoria');
+            $table->foreign('id_categoria')->references('id')->on('livro_categorias')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('qtd_disponivel');
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_12_230046_create_emprestimos_table.php
+++ database/migrations/2024_03_12_230046_create_emprestimos_table.php
@@ -9,10 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // livro emprestado, data de empréstimo, data de devolução, usuário que retirou o livro
     public function up(): void
     {
         Schema::create('emprestimos', function (Blueprint $table) {
             $table->id();
+            $table->date('data_de_emprestimo');
+            $table->date('data_de_devolucao');
+
+            $table->unsignedBigInteger('id_livro');
+            $table->foreign('id_livro')->references('id')->on('livros');
+
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            
             $table->timestamps();
         });
     }
