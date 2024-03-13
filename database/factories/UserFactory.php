9c6533e26139ea0b8624880eb3ee2c508249afc9
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-12 20:53:28 -0400

    user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstName' => fake()->firstName(),
+            'lastName' => fake()->lastName(),
+            'endereco' => fake()->address(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'telefone' => fake()->phoneNumber(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
