eb2d649a5dad11361643df3103d1dfe06bad143f
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-13 16:50:40 -0400

    resource route

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ use App\Http\Controllers\UserController;
 Route::get('/', action:[HomeController::class, 'index'])->name('home');
 
 Route::get('/users', action:[UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', action:[UserController::class, 'create'])->name('users.create');
-Route::post('/users', action:[UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}', action:[UserController::class, 'show'])->name('users.show');
-Route::get('/users/{user}/edit', action:[UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', action:[UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', action:[UserController::class, 'destroy'])->name('users.destroy');
+// Route::get('/users/create', action:[UserController::class, 'create'])->name('users.create');
+// Route::post('/users', action:[UserController::class, 'store'])->name('users.store');
+// Route::get('/users/{user}', action:[UserController::class, 'show'])->name('users.show');
+// Route::get('/users/{user}/edit', action:[UserController::class, 'edit'])->name('users.edit');
+// Route::put('/users/{user}', action:[UserController::class, 'update'])->name('users.update');
+// Route::delete('/users/{user}', action:[UserController::class, 'destroy'])->name('users.destroy');
+
+Route::resource('users', UserController::class);
