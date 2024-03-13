83513a7a029b32aeb8c96a78d9947e07817b9eb6
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-13 19:18:22 -0400

    updating categorias migration

--- database/seeders/CategoriasSeeder.php
+++ database/seeders/CategoriasSeeder.php
@@ -5,15 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use App\Models\LivroCategoria;
+use App\Models\Categoria;
 
-class LivroCategoriasSeeder extends Seeder
+class CategoriasSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        LivroCategoria::factory(5)->create();
+        Categoria::factory(5)->create();
     }
 }
