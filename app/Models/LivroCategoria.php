33165d68e4228784816dc24158f8ffdccc336559
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-12 20:05:16 -0400

    livro categoria factory

--- app/Models/LivroCategoria.php
+++ app/Models/LivroCategoria.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class LivroCategoria extends Model
 {
     use HasFactory;
+
+    protected $table = 'livro_categorias';
 }
---
+++ database/factories/LivroCategoriaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LivroCategoria>
+ */
+class LivroCategoriaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => $this->faker->unique()->word,
+            'descricao' => $this->faker->text,
+        ];
+    }
+}
