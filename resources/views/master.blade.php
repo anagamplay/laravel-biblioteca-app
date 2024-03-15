ec8270fd1a667c32b44d0014dba1c5b58e108379
AnaGamplay <anaclarasilvestre360@gmail.com>
2024-03-15 14:41:03 -0400

    homepage

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,26 +10,26 @@
 <body>
     <header>
         <div class="row" id="myHeader">
-        <h1>BIBLIOTECA</h1>
+            <h1>BIBLIOTECA</h1>
         </div>
     </header>
 
     <nav class="p-3 text-bg-light" id="myNavbar">
         <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-            </a>
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                </a>
 
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="{{ route('home') }}" class="nav-link px-2 text-secondary">Home</a></li>
-            </ul>
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="{{ route('home') }}" class="nav-link px-2 text-secondary">Home</a></li>
+                </ul>
 
-            <div class="text-end">
-            <button type="button" class="btn btn-outline-dark me-2">Login</button>
-            <button type="button" id="myButton" class="btn btn-warning">Sign-up</button>
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-dark me-2">Login</button>
+                    <button type="button" id="myButton" class="btn btn-warning">Sign-up</button>
+                </div>
             </div>
-        </div>
         </div>
     </nav>
 
